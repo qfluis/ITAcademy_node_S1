0357da8d7d8857e9ef13471d7fcb012747ab28ed
Luis Quevedo <qfluis@gmail.com>
2022-04-19 09:35:58 +0200

    Entrega 1.5 - Ejercicio 2 Nivel 2 - Simplificado

--- 1.5-ContenidoDirectorio.js
+++ 1.5-ContenidoDirectorio.js
@@ -2,6 +2,21 @@
 /* Crea una funció que llisti per la consola el contingut del directori d'usuari de 
 l'ordinador utilizant Node Child Processes. */
 
+const os = require("os");
+const fs = require('fs');
+
+const urlUser = os.homedir();
+
+const files = fs.readdir(urlUser, (err, files) =>{
+    if (err) console.log('Error', err.message);
+    else console.log(`Contenido del directorio de usuario (${urlUser}):`, files);
+});
+
+
+
+
+
+/* OLD COMPLICATED IMPLEMENTATION
 // Sólo funciona en windows 
 // además el fichero tiene que estar dentro de la carpeta de usuario (en cualquier nivel)
 // No he encontrado una manera mejor de hacerlo (supongo que la hay...)
@@ -14,3 +29,4 @@ const files = fs.readdir(urlUser, (err, files) =>{
     if (err) console.log('Error', err.message);
     else console.log('Contenido del directorio:', files);
 });
+*/
